abdde2ddd5b135d5c517a5689d478c35c2e48176
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-21 13:48:56 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("Homework Day 4");
+
+//Verbal questions
+//1. a parameter is defined in the function and describes what
+//the argument will be inputed by the user and will be evaluated
+//by the function
+
+//2. a console log is simply a debugging tool that doesn't 
+//accomplish anything; return actually "returns" us a value,
+//it's the output of a function
+
+//3. the implications of the ability of a function to return
+//a value is that a user is able to input a value on the front
+//end, the function will evaluate it based on the block of code
+//it's programmed to run, and then the user will actually SEE
+//the result. furthermore, the result can then be redefined into
+//a new variable that can be used elsewhere
