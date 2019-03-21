b5fc5821fde70ff7488dc0fa98e8e331a6d37813
Adam Wolfman <adamwolfman@Adams-MacBook-Pro.local>
2019-03-21 15:20:59 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log('i am loaded');
+
+// PROBLEM SOLVING WITH FUNCTIONS
+
+//1. VERBAL QUESTIONS
+
+//1. What is the difference between a parameter and an argument. 
+//   Parameter is what you use as the placeholders for the data that will be input in
+//   your function
+//   Arguments are the data that is passed in to the function in place of the parameters
+
+//2. Return gives you the output of a function. It does not print anything to the console
+//   or inherently do anything until called. 
+
+//   console.log() will log whatever is passed in to the console. It does not actually do anything
+//   other than print to the console and its output cannot be assigned to a variable. 
+
+//3. When you assign a function to a variable, you are then able to use the function
+//   to do stuff. This allows you to use functions in other functions and in other 
+//places around the page. 
+
