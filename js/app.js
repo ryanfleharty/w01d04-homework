17273af0048c067ec372e8547ae6ffa89d60f86c
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-21 15:45:24 -0600

    Commit 2 - Palindrome

--- js/app.js
+++ js/app.js
@@ -15,3 +15,28 @@ console.log("Homework Day 4");
 //it's programmed to run, and then the user will actually SEE
 //the result. furthermore, the result can then be redefined into
 //a new variable that can be used elsewhere
+
+//Palindrome again
+
+const checkPalindrome = (palindrome) => {
+	return palindrome == palindrome.split('').reverse().join('');
+}
+
+console.log(checkPalindrome('racecar'));
+console.log(checkPalindrome('borscht'));
+
+//.split is a string method 
+//"" is going to separate everything and return you an array
+//now that we have the array, .reverse will reverse it
+//.join is another array method that will put it back together
+//we then will compare the original word to the new output
+
+//OR the answer that makes MORE sense...
+//	 for(let i = 0; i <str.length; i++){
+	//if(str[i] !== str[str.length -1 -i])
+		//return false
+//}
+
+
+
+
