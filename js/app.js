72175cb6ac4f35c1512ea67b19e405ea6b9d0946
keenmaster486 <cebrock106@gmail.com>
2019-03-21 14:50:59 -0600

    Commit 2 - Palindrome

--- js/app.js
+++ js/app.js
@@ -10,3 +10,22 @@ console.log("app.js loaded");
 //3. You can use functions to serve as reusable containers for code
 //that you will be using frequently.
 
+const checkPalindrome = function(s)
+{
+	//s is the string we are checking
+
+	//Make the string all uppercase:
+	s = s.toUpperCase();
+
+	for (let i = 0; i < s.length; i++)
+	{
+		if (!(s[i] == s[s.length - i - 1])) //that is, if we found a non-matching letter
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Borscht"));
