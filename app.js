982b109af8727342de8dc4669a49b160b7c1fa5c
Sami Austin <samicaustin@gmail.com>
2019-03-21 10:44:45 -0600

    Commit 1 - Verbal Questions

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Homework page ready!")
+
+//1. A parameter is a variable within a method definition.
+// An argument is the value a parameter holds.
+
+//2. A return will end a function and (sometimes) returns a value
+// while a console.log doesn't interact with your code, it just
+// will log a value into the browser console if it gets called.
+
+//3. If a function returns a value, the user knows that the function
+// has been completed.
