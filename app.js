b3eba314032832f691566c8910d721d9342ee86c
Sami Austin <samicaustin@gmail.com>
2019-03-21 15:10:55 -0600

    Commit 2 - Palindrome

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Homework page ready!")
-
+//Verbal Questions
 //1. A parameter is a variable within a method definition.
 // An argument is the value a parameter holds.
 
@@ -8,4 +8,20 @@ console.log("Homework page ready!")
 // will log a value into the browser console if it gets called.
 
 //3. If a function returns a value, the user knows that the function
-// has been completed.
+// has been completed and is no longer running.
+
+//Palindrome Again
+const checkPalindrome = (word) => {
+    let backwards = word.split("").reverse().join("");
+    console.log(backwards);
+    if (backwards === word) {
+        console.log("It's a palindrome!");
+    }
+  } 
+
+  checkPalindrome("mom");
+
+// Digital Sum 
+const sumDigits = (num) => {
+    
+}
