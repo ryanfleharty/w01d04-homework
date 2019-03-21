9f2b95ce7f2d3e62a9e74df6dd8b11ac746f1c10
John Oro <johnmichaeloro@gmail.com>
2019-03-21 15:20:22 -0600

    Verbal Questions answered

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Verbal Questions
+//What is the difference between a parameter and an argument?
+//Let me see if I get this right. A function is a method. A method has a parameter, which is a variable that can contain information. The information the variable contains is called the argument. When you call a function, the argument is the data you provide so the function has something to work with.
+
+//Within a function, what is the difference between return and console.log?
+//When you console log something, you're usually asking the console to print the outcome of a function or the contents of a variable. It's not really producing anything within the function or the larger file. (Think of it as a way to debug.) Return causes the function to produce an output, a value that leaves the function and can be used in the file.
+
+//What are the implications of the ability of a function to return a value?
+//It all comes down to scope. You can store a variable inside of a function. This tightens the scope of that variable to the block of the function. You can have the function return that variable. Then you can access the variable later by invoking the function. It's a very clean way of coding that keeps your scope tight.
