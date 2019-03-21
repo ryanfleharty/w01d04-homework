fca2b2386c16bd6c96fd8ee68e5bc2d8d3e9aad5
keenmaster486 <cebrock106@gmail.com>
2019-03-21 12:14:20 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("app.js loaded");
+
+
+//1. A parameter is a variable declared in the function definition,
+//whereas an argument is what you actually pass to the function.
+
+//2. Return sets the value of the function and exits. console.log merely
+//prints something to the console.
+
+//3. You can use functions to serve as reusable containers for code
+//that you will be using frequently.
+
