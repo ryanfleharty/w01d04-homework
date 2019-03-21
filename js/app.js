60ffb31b7c8163ee12e1d36d3757c2b65970d17d
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-21 16:41:14 -0600

    Commit 5 - Sum Array

--- js/app.js
+++ js/app.js
@@ -63,7 +63,18 @@ const calculateSide = (sideA, sideB) => {
 
 console.log(calculateSide(8,6));
 
-//
+//Sum Array
+
+const sumArray = (arr) => {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++){
+		sum += arr[i]
+	}
+	return sum;
+}
+
+console.log(sumArray([8, 2, 3, 4, 5, 6]));
+
 
 
 
