f206d78fadd0c22ecbdb8c0eeefe967b5350194b
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-03-21 10:37:15 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log('Up and running');
+
+// 1. Verbal questions
+// Write answers to the following questions as comments.
+// 1. What is the difference between a parameter and an argument?
+// A parameter is defined in the function declaration to indicate what should be input when calling it
+// An argument is the actual value put into the parameters when invoking a function.
+// 2. Within a function, what is the difference between return and console.log?
+// return commend passes whatever is placed in it back to the calling location; console.log only prints values to the console.
+// 3. What are the implications of the ability of a function to return a value?
+// If a function can return a value, it can then be used by the calling location, functions allow for DRY modularized code.
+
+// 2. Palindrome again.
+// Write a function checkPalindrome that accepts a single argument, a string. Yes, you've done it before, but do it again. Later in this assignment we're gonna beef up our palindrome function some. See if you can do it without looking back at your previous answer. The function should return true if the string is a palindrome, false if not. Make sure your function will give the correct answer for words with capital letters.
+const checkPalindrome == (str) => {
+
+}
