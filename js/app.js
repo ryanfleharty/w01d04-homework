7e35a5c0078b7755760e2da81b00835c2fe51f98
shadiboi <saoutabachi@gmail.com>
2019-03-21 15:57:50 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+// ## 1. Verbal questions
+
+// 1. What is the difference between a **parameter** and an **argument**?
+                //A parameter is a variable in a method or function that will later be given a value to through arguments. 
+                //When a method is called, the arguments are the data you pass into the method's parameters.
+// 2. Within a function, what is the difference between **return** and **console.log**?
+                //return will actully spit something back at you and end the functoin.WHile 
+                //console.log will only log your result to the console but wont do anything with it
+// 3. What are the implications of the ability of a function to return a value?
+                //once it returns a value the finction will end
+
+
