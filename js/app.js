482d249c4ca849a5e39fe33a114a9f9eea5fec81
shadiboi <saoutabachi@gmail.com>
2019-03-21 22:32:20 -0600

    3-21-functions-scope

--- js/app.js
+++ js/app.js
@@ -12,3 +12,55 @@ console.log("hello");
                 //once it returns a value the finction will end
 
 
+// ## 2. Palindrome again.
+// Write a function `checkPalindrome` that accepts a single argument, a string. Yes, you've done it before, 
+//but do it again. Later in this assignment we're gonna beef up our palindrome function some. See if you can do it without 
+//looking back at your previous answer. The function should return true if the string is a palindrome, 
+//false if not. Make sure your function will give the correct answer for words with **capital letters**.
+
+
+    
+// const checkPalindrome = (str) => {
+let str = "alabama";
+let str 2 = strstr.split(" ");
+
+ 
+//     for (let i = 0; i < str.length -2; i++) {
+//             if (str[i] !== str[str.length -1 -i]) {
+//                 console.log("false")
+//             } else {
+//                 console.log("true")
+//             }};
+            
+// checkPalindrome();        
+    
+    //checkPalindrome("whatsupbro");
+    
+    // console.log(checkPalindrome("Radar"));
+    // console.log(checkPalindrome("Borscht"));
+    
+    // ## 3. Digit Sum
+    // Write a function `sumDigits` that accepts a number and returns the sum of its digits.
+    
+    
+    
+    // const sumDigits = (num) => {  
+    //     let strNum = num.toString()                  
+    //     for (let i = 0; i < num.length; i++) {   //MY WORK
+    //         num.toString();
+    // }};
+    
+    
+    // sumDigits(52);
+                                
+    
+    // console.log(sumDigits(42));
+    // => 6;
+    // ```
+    
+    // <hr>
+    // &#x1F534; **Commit your work.** <br>
+    // The commit message should read: <br>
+    // "Commit 3 - Digit Sum".
+    // <hr></hr>
+    
