9630cc71ee8a2f869216e81b7dfb8afbdc96ef65
Destiny <dshillis1@gmail.com>
2019-03-21 21:48:50 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//Problem solving with Functions
+
+//1. Verbal questions
+//1. What is the difference between a parameter and an argument?
+//A perameter is the variable you put between the () within a function.
+//ex. (num, str, arr)
+//An argument is the value you put in the () when you call a function.
+//ex. function(123); 
+
+//2. Within a function, what is the difference between return and 
+//console.log?
+//Return actually outputs the value to the interface.
+//console.log is a way to check and debug code only seen by programmers.
+
+//3. What are the implications of the ability of a function to 
+//return a value?
+//return actually outputs the value so it can be stored as a variable 
+//and used later. 
+
+
