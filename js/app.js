56f96977efc29dfd02c890beb8de112881e22394
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-03-21 19:02:29 -0600

    Commit 4- pythagoras

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//1. Verbal Questions
+//a parameter are listed as part of a function defintion
+// arguements are values passed to a function when its invoked or called upon
+
+//2
+//console.logs are more for the developer, it is something that helps us debug and check our work (ie true/false)
+//returns do the thing, we cant really see them but they perform the function
+
+//3. not entirely sure what the question is asking
+//the functions job is to return some type of value to make something happen or work
+// can also be used and reffered back to many times, as long as its invoked.
+
+
+//Palindrome 
+function checkPalindrome(str) {
+if (str == str.split(''). reverse().join('')
+	return true;
+} else {
+	return false;
+}
+
+checkPalindrome("trashcan");
+checkPalindrome("racecar");
+
+
+//Digit sum
+const sumDigits = 
+
+
+
+
+
+
+//Commit 4- Pythagoras
+const calcuteSide = (sideA, sideB) => {
+return Math.floor(Math.sqrt((a * a) + (b * b)))
+}
+
+calcuteSide(4,5);
+
