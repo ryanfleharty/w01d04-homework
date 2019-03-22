198bb7d991d8f346e6c1b2c30a9909679bebe706
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-03-21 21:01:15 -0600

    w01d04 HW completed

--- js/app.js
+++ js/app.js
@@ -25,4 +25,69 @@ console.log(checkPalindrome("radar"));
 console.log(checkPalindrome("letter"));
 
 
-//3.) CALCULATE
+//3.) DIGIT SUM
+sumDigits = (number) => {
+	sum = 0
+
+	while (number) {
+    	sum += number % 10; // taking last number, pulling it down and repeating
+    	number = Math.floor(number / 10);
+	}
+	return sum;		
+} 
+	
+
+console.log("diget sum = " + sumDigits(42));
+
+
+// 4.) PYTHAGORAS
+calculateSide = (sideA, sideB) =>{
+	output = sideA * sideA;
+	output += sideB * sideB; // adding sideA output to sideB
+
+	return output;
+
+}
+console.log("pythagoras = " + calculateSide(8, 6));
+
+sumArray = (arr) =>{
+	sum = 0;
+	for(i=0; i < arr.length; i++){
+		sum += arr[i];
+	}
+	return sum;
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+// CHECKPRIME
+checkPrime = (number) =>{
+	
+	output = false
+	for(i=1; i < number; i++){
+		//alert(i*i)
+		if (i * i == number){
+			output = true
+		}
+	} 
+	return output;
+}
+console.log(checkPrime(9));
+
+// PART 2 PRINTPRIMES
+printPrimes = (number) =>{
+
+	for(i=0; i < number; i++){
+		
+		if (checkPrime(i))
+			console.log(i)
+	}
+
+}
+printPrimes(97);
+
+
+
+
+
+
+
